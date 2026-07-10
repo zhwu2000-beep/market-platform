4d376964e7e134144ce796ac982db2363a16dd0f
zhwu2000-beep <zhwu2000@gmail.com>
2026-07-11 03:30:47 +0800

    Add relative strength signal

--- src/market_platform/signals/__init__.py
+++ src/market_platform/signals/__init__.py
@@ -8,15 +8,21 @@ from market_platform.signals.calculators import (
     calculate_trend,
 )
 from market_platform.signals.models import MarketSignal, MarketSignalSnapshot
+from market_platform.signals.relative_strength import (
+    align_asset_and_benchmark_prices,
+    calculate_relative_strength,
+)
 from market_platform.signals.service import calculate_market_signals
 
 __all__ = [
+    "align_asset_and_benchmark_prices",
     "MarketSignal",
     "MarketSignalSnapshot",
     "calculate_current_drawdown",
     "calculate_distance_from_moving_average",
     "calculate_market_signals",
     "calculate_momentum",
+    "calculate_relative_strength",
     "calculate_realized_volatility",
     "calculate_trend",
 ]
